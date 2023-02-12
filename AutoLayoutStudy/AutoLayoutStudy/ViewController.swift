ea6256d96858a777efb69b482a4c11a95a8c0c3e
donnyrla10 <kokoo1214@naver.com>
2023-02-12 18:00:07 +0900

    first setting

---
+++ AutoLayoutStudy/AutoLayoutStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutStudy
+//
+//  Created by 김영선 on 2023/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
